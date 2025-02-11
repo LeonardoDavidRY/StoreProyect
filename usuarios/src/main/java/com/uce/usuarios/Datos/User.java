29bbda1678381c5cc6e67cd75e5e92601a5523c2
Mateojami <jamimateo8@gmail.com>
2025-02-11 12:47:19 -0500

    Creando usuarios

---
+++ usuarios/src/main/java/com/uce/usuarios/Datos/User.java
@@ -0,0 +1,28 @@
+package com.uce.usuarios.Datos;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String role;  // Ejemplo: "ADMIN" o "USER"
+}
